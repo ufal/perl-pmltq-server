2436110b5f88ba821d0ed47958481f1839a697d6
Michal Sedlák <sedlakmichal@gmail.com>
2014-08-16 11:33:12 +0200

    Add MongoDB connection helper

--- lib/PMLTQ/Server.pm
+++ lib/PMLTQ/Server.pm
@@ -1,18 +1,57 @@
 package PMLTQ::Server;
 use Mojo::Base 'Mojolicious';
 
+use Mango;
+use Mango::BSON ':bson';
+
+has db => sub { state $mango = Mango->new(shift->config->{mongo_uri}) };
+
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+  $self->plugin('Config' => {
+    file => $self->home->rel_file('config/pmltq_server.conf')
+  });
+
+	$self->helper(mango => sub { shift->app->db });
 
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to('Auth#login');
+
+  $r->get('/mongotest' => sub {
+    my $c = shift;
+
+    my $collection = $c->mango->db->collection('visitors');
+    my $ip         = $c->tx->remote_address;
+
+    # Store information about current visitor
+    $collection->insert({when => bson_time, from => $ip} => sub {
+      my ($collection, $err, $oid) = @_;
+
+      return $c->render_exception($err) if $err;
+
+      # Retrieve information about previous visitors
+      $collection->find->sort({when => -1})->fields({_id => 0})->all(sub {
+        my ($collection, $err, $docs) = @_;
+
+        return $c->render_exception($err) if $err;
+
+        # And show it to current visitor
+        $c->render(json => $docs);
+      });
+    });
+
+    $c->render_later;
+  })
+}
+
+
+sub setup_helpers {
+
 }
 
 1;
