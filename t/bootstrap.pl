e558d821490bc40d5ee591e8f98141a1a1523d90
Michal Sedlák <sedlakmichal@gmail.com>
2014-09-03 13:54:52 +0200

    Testing database script
    Start testing postgres database by running ./t/scripts/pg_testing
    
    This (for now) requires modified version of Test::PostgreSQL found at
    m1ch4ls/Test-postgresql

--- t/bootstrap.pl
+++ t/bootstrap.pl
@@ -1,25 +1,40 @@
 # bootstraping tests
 use Mojo::Base -strict;
+use Carp::Always;
+use File::Basename 'dirname';
+use File::Spec;
 
 use Test::More;
+use lib File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..', '..', 'Test-postgresql', 'lib'));
 use Test::PostgreSQL;
 use Test::Mojo;
 
 use Treex::PML;
-
-use File::Basename 'dirname';
-use File::Spec;
 use File::Which qw( which );
 
 use lib File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..', 'lib'));
+use lib File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..', '..', 'perl-pmltq', 'lib'));
 
 my $test_files = File::Spec->catdir(dirname(__FILE__), 'test_files');
+my $pg_dir = File::Spec->catdir(dirname(__FILE__), 'postgres');
+my $pg_local = -d $pg_dir && $ENV{LOCAL_PG};
+my $pg_port = $ENV{PG_PORT};
+my ($pg_restore, $pgsql);
 
-my $pgsql = Test::PostgreSQL->new()
-    or plan skip_all => $Test::PostgreSQL::errstr;
+unless ($pg_port) {
+  $pgsql = Test::PostgreSQL->new(
+    auto_start => 0,
+    ($ENV{LOCAL_PG} ? (base_dir => $pg_dir) : ()), # use dir for subsequent runs to simply skip initialization
+  ) or plan skip_all => $Test::PostgreSQL::errstr;
 
-my $pg_restore = which('pg_restore');
-die "Cannot find pg_restore in your path" unless $pg_restore;
+  $pgsql->setup() unless (-d $pg_dir);  # create postgress dir does not exists
+
+  $pgsql->start;
+    
+  $pg_port = $pgsql->port;  
+  $pg_restore = which('pg_restore');
+  die "Cannot find pg_restore in your path" unless $pg_restore;
+}
 
 Treex::PML::AddResourcePathAsFirst(File::Spec->catdir($test_files, 'resources'));
 
@@ -38,7 +53,7 @@ sub test_treebank {
     title => 'PDT 2.0 Sample',
     driver => 'Pg',
     host => 'localhost',
-    port => $pgsql->port,
+    port => $pg_port,
     database => 'test',
     username => 'postgres',
     password => '',
@@ -53,13 +68,26 @@ sub test_treebank {
   });
 
   $test_tb->save();
-  my $filename = File::Spec->catdir($test_files, 'pdt20_mini', 'pdt20_mini.dump');
 
-  my @cmd = ($pg_restore, '-d', 'test', '-h', 'localhost', '-p', $pgsql->port, '-U', 'postgres', '--no-acl', '--no-owner', '-w', $filename);
-  say STDERR join(' ', @cmd);
-  system(@cmd) == 0 or die "Restoring test database failed: $?";
+  init_database();
 
   return $test_tb
+}
+
+sub init_database {
+  return if $pg_local; 
+
+  my $filename = File::Spec->catdir($test_files, 'pdt20_mini', 'pdt20_mini.dump');
+
+  my @cmd = ($pg_restore, '-d', 'test', '-h', 'localhost', '-p', $pg_port, '-U', 'postgres', '--no-acl', '--no-owner', '-w', $filename);
+  say STDERR join(' ', @cmd);
+  system(@cmd) == 0 or die "Restoring test database failed: $?";
+}
+
+sub run_database {
+  return unless $pgsql;
+  say STDERR "Connect to: " . $pgsql->dsn;
+  sleep;
 }
 
 1;
