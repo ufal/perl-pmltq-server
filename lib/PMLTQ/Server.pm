051168bbd1c77dcc72dbe30334773eda2e74eab4
Michal Sedlák <sedlakmichal@gmail.com>
2014-09-12 10:27:22 +0200

    Clean up routes

--- lib/PMLTQ/Server.pm
+++ lib/PMLTQ/Server.pm
@@ -81,7 +81,7 @@ sub startup {
       return;
     }
     $c->redirect_to($c->url_for('admin_welcome'));
-  });
+  })->name('home');
 
   # Autheticate routes
   my $auth = $r->route('/auth')->to(controller => 'Auth');
@@ -95,25 +95,6 @@ sub startup {
 
   my $profile = $r->get('/profile')->over(authenticated => 1)->to('Profile#index');
   $profile->any([qw/GET POST/] => 'update')->over(has_priv => 'selfupdate')->to('Profile#update');
-
-  # $r->get('/admin/user/test')       ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#testuserexist');
-  # $r->get('/admin/user/list')       ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#listuser');
-  # $r->post('/admin/user/add')       ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#adduser');
-  # $r->get('/admin/user/add')        ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#adduser_form');
-  # $r->get('/admin/user/delete')     ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#deluser');
-  # $r->post('/admin/user/update')    ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#updateuser');
-  # $r->get('/admin/user/update')     ->over(authenticated => 1, has_priv => 'admin')     ->to('Admin#updateuser_form');
-
-  # $r->post('/profile/selfupdate')->over(authenticated => 1, has_priv => 'selfupdate')->to('Profile#update');
-  # $r->get('/profile/selfupdate') ->over(authenticated => 1, has_priv => 'selfupdate')->to('Admin#update_form');
-  # $r->get('/profile')            ->over(authenticated => 1)                          ->to('Profile#index');
-
-  # $r->get('/admin/treebank/list')    ->over(authenticated => 1, has_priv => 'admin')->to('Admin#listtreebank');
-  # $r->post('/admin/treebank/add')    ->over(authenticated => 1, has_priv => 'admin')->to('Admin#addtreebank');
-  # $r->get('/admin/treebank/add')     ->over(authenticated => 1, has_priv => 'admin')->to('Admin#addtreebank_form');
-  # $r->get('/admin/treebank/delete')  ->over(authenticated => 1, has_priv => 'admin')->to('Admin#deltreebank');
-  # $r->post('/admin/treebank/update') ->over(authenticated => 1, has_priv => 'admin')->to('Admin#updatetreebank');
-  # $r->get('/admin/treebank/update')  ->over(authenticated => 1, has_priv => 'admin')->to('Admin#updatetreebank_form');
 
   my $treebank = $r->bridge('/:treebank')->
     name('treebank')->to(controller => 'Treebank', action => 'initialize');
