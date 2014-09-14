09f4396015eff15cfcaa6ae438d6c5cae6047293
Matyas Kopp <koppmaty@fit.cvut.cz>
2014-09-15 00:24:09 +0200

    fix treebank controllerS

--- lib/PMLTQ/Server/Controller/Admin/Treebank.pm
+++ lib/PMLTQ/Server/Controller/Admin/Treebank.pm
@@ -13,41 +13,7 @@ Bridge for other routes. Saves current treebank to stash under 'tb' key.
 
 =cut
 
-sub initialize {
-  my $c = shift;
 
-  my $tb_name = $c->param('treebank');
-  $c->mandel->collection('treebank')->search({name => $tb_name})->single(sub {
-    my ($collection, $err, $tb) = @_;
-
-    unless ($tb) {
-      $c->status_error({
-        code => 404,
-        message => "Treebank '$tb_name' not found"
-      });
-      return;
-    }
-
-    if ($err) {
-      $c->status_error({
-        code => 500,
-        message => "Database error: $err"
-      });
-      return;
-    }
-
-    $c->stash(tb => $tb);
-    $c->continue;
-  });
-
-  return undef;
-}
-
-sub metadata {}
-
-sub suggest {}
-
-# crud
 
 sub list {
   my $c = shift;
