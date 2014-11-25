b33cc3813617ae27085a550a9b126e473db35f95
Michal Sedlák <sedlakmichal@gmail.com>
2014-11-25 14:00:52 +0100

    Turn permissions into constants

--- lib/PMLTQ/Server/Model/Permission.pm
+++ lib/PMLTQ/Server/Model/Permission.pm
@@ -2,5 +2,17 @@ package PMLTQ::Server::Model::Permission;
 
 use PMLTQ::Server::Document 'permissions';
 use Types::Standard qw(Str);
+use Exporter 'import';
+
+BEGIN {
+  use constant {
+    map { ( uc $_ => $_ ) } qw/admin all_treebanks shibboleth/
+  };
+
+  our @EXPORT = map { uc } qw/admin all_treebanks shibboleth/;
+  our %EXPORT_TAGS = (
+    constants => [ @EXPORT ],
+  );
+}
 
 field [qw/name comment/] => (isa => Str);
