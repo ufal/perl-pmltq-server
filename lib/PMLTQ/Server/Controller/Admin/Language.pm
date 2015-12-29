6fd9ae2658e10a609edf62eb8d7b320d731bf73d
Michal Sedlák <sedlakmichal@gmail.com>
2015-12-29 15:18:21 +0100

    Add support for langauge crud

--- lib/PMLTQ/Server/Controller/Admin/Language.pm
+++ lib/PMLTQ/Server/Controller/Admin/Language.pm
@@ -1,9 +1,29 @@
 package PMLTQ::Server::Controller::Admin::Language;
 
-# ABSTRACT: Managing tags in administration
+# ABSTRACT: Managing languages in administration
 
 use Mojo::Base 'PMLTQ::Server::Controller::CRUD';
 
 has resultset_name => 'Language';
+
+has search_fields => sub { [qw/name code/] };
+
+sub _validate {
+  my ($c, $language_data) = @_;
+
+  my $rules = {
+    fields => [qw/id language_group_id code name/],
+    filters => [
+      # Remove spaces from all
+      [qw/name code/] => filter(qw/trim/),
+    ],
+    checks => [
+      [qw/name language_group_id code/] => is_required(),
+      name => is_long_at_most(120),
+      code => [is_long_at_most(10), is_unique($c->resultset, 'id', 'language code already exists')],
+    ]
+  };
+  return $c->do_validation($rules, $language_data);
+}
 
 1;
---
+++ lib/PMLTQ/Server/Controller/Admin/LanguageGroup.pm
@@ -0,0 +1,9 @@
+package PMLTQ::Server::Controller::Admin::LanguageGroup;
+
+# ABSTRACT: Managing language groups in administration
+
+use Mojo::Base 'PMLTQ::Server::Controller::CRUD';
+
+has resultset_name => 'LanguageGroup';
+
+1;
