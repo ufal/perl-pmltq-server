28d17d0af26b81c7882f83f2c4b73f2cbc8f8a8e
Matyáš Kopp <matyas.kopp@gmail.com>
2017-02-14 15:24:01 +0100

    check whether is login type allowed added empty sign_in_ldc function

--- lib/PMLTQ/Server/Controller/Auth.pm
+++ lib/PMLTQ/Server/Controller/Auth.pm
@@ -55,6 +55,11 @@ sub is_admin {
 sub sign_in {
   my $c = shift;
 
+  unless ($c->config->{login_with}->{local}) {
+    $c->rendered(404);
+    return;
+  }
+
   my $auth_data = $c->req->json('/auth') || {};
   #print STDERR $c->dumper($c->req->json);
   if(($auth_data = $c->_validate_auth($auth_data))
@@ -69,11 +74,27 @@ sub sign_in {
   }
 }
 
+sub sign_in_ldc {
+  my $c = shift;
+
+  unless ($c->config->{login_with}->{ldc}) {
+    $c->rendered(404);
+    return;
+  }
+
+  ## TODO
+}
+
 # TODO: check Shibboleth attributes to actually create user accounts
 sub sign_in_shibboleth {
   my $c = shift;
 
   unless ($c->config->{shibboleth}) {
+    $c->rendered(404);
+    return;
+  }
+
+  unless ($c->config->{login_with}->{shibboleth}) {
     $c->rendered(404);
     return;
   }
