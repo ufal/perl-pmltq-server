743f87d75579976c7ca35c2782fbe1d38efee185
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2020-01-22 16:50:04 +0100

    add valid_until column to user

---
+++ share/migrations/SQLite/downgrade/11-10/001-auto.sql
@@ -0,0 +1,64 @@
+-- Convert schema '/home/matyas/Documents/UFAL/REP/perl-pmltq-server/script/../share/migrations/_source/deploy/11/001-auto.yml' to '/home/matyas/Documents/UFAL/REP/perl-pmltq-server/script/../share/migrations/_source/deploy/10/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+CREATE TEMPORARY TABLE users_temp_alter (
+  id INTEGER PRIMARY KEY NOT NULL,
+  persistent_token varchar(250),
+  organization varchar(250),
+  provider varchar(250),
+  name varchar(120),
+  username varchar(120),
+  email varchar(120),
+  password varchar(60),
+  access_all boolean NOT NULL DEFAULT 0,
+  is_admin boolean NOT NULL DEFAULT 0,
+  is_active boolean NOT NULL DEFAULT 0,
+  created_at datetime NOT NULL,
+  last_login datetime
+);
+
+;
+INSERT INTO users_temp_alter( id, persistent_token, organization, provider, name, username, email, password, access_all, is_admin, is_active, created_at, last_login) SELECT id, persistent_token, organization, provider, name, username, email, password, access_all, is_admin, is_active, created_at, last_login FROM users;
+
+;
+DROP TABLE users;
+
+;
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY NOT NULL,
+  persistent_token varchar(250),
+  organization varchar(250),
+  provider varchar(250),
+  name varchar(120),
+  username varchar(120),
+  email varchar(120),
+  password varchar(60),
+  access_all boolean NOT NULL DEFAULT 0,
+  is_admin boolean NOT NULL DEFAULT 0,
+  is_active boolean NOT NULL DEFAULT 0,
+  created_at datetime NOT NULL,
+  last_login datetime
+);
+
+;
+CREATE INDEX idx_name02 ON users (username);
+
+;
+CREATE INDEX idx_external02 ON users (persistent_token, organization, provider);
+
+;
+CREATE UNIQUE INDEX user_username_unique02 ON users (name);
+
+;
+INSERT INTO users SELECT id, persistent_token, organization, provider, name, username, email, password, access_all, is_admin, is_active, created_at, last_login FROM users_temp_alter;
+
+;
+DROP TABLE users_temp_alter;
+
+;
+
+COMMIT;
+
